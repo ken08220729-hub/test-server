c66bf402c53e763d7c919daa2f8a62995157434b
ken08220729-hub <ken08220729@gmail.com>
2026-06-19 18:41:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
     next();
 });
 
+let count = 0;
+
 app.get("/", (req, res) => {
     res.send("伺服器正常");
+});
+
+app.get("/count", (req, res) => {
+    res.json({ count });
+});
+
+app.post("/add", (req, res) => {
+    count++;
+    res.json({ count });
 });
 
 const PORT = process.env.PORT || 3000;
