6f6e248f1a05440d2fb7fdbfc9d9fc20f3fd9a87
ken08220729-hub <ken08220729@gmail.com>
2026-06-19 23:17:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,31 +4,54 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
     cors: {
         origin: "*"
     }
 });
 
-let count = 0;
+let players = {};
 
 io.on("connection", (socket) => {
-    console.log("玩家連線:", socket.id);
 
-    // 進來先送目前數字
-    socket.emit("update", count);
+    socket.on("choice", (choice) => {
 
-    // 玩家按 +1
-    socket.on("add", () => {
-        count++;
-        io.emit("update", count); // 全部同步
+        players[socket.id] = choice;
+
+        let ids = Object.keys(players);
+
+        if (ids.length >= 2) {
+
+            let p1 = players[ids[0]];
+            let p2 = players[ids[1]];
+
+            let result = "";
+
+            if (p1 === p2) {
+                result = "平手";
+            }
+            else if (
+                (p1 === "石頭" && p2 === "剪刀") ||
+                (p1 === "剪刀" && p2 === "布") ||
+                (p1 === "布" && p2 === "石頭")
+            ) {
+                result = "玩家1獲勝";
+            }
+            else {
+                result = "玩家2獲勝";
+            }
+
+            io.emit("result", {
+                p1,
+                p2,
+                result
+            });
+
+            players = {};
+        }
     });
 
-    socket.on("disconnect", () => {
-        console.log("離開:", socket.id);
-    });
 });
 
-server.listen(process.env.PORT || 3000, () => {
-    console.log("Server running");
-});
+server.listen(process.env.PORT || 3000);
