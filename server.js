8ea83d353b3b83ff0a98e3b4ffc5c7d3c871ad91
ken08220729-hub <ken08220729@gmail.com>
2026-06-19 23:44:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,35 +11,55 @@ const io = new Server(server, {
     }
 });
 
+// 在線人數
+let onlinePlayers = 0;
+
+// 暫存出拳
 let players = {};
 
 io.on("connection", (socket) => {
 
+    console.log("玩家連線:", socket.id);
+
+    // 在線人數+1
+    onlinePlayers++;
+
+    // 通知所有玩家
+    io.emit("onlineCount", onlinePlayers);
+
+    // 收到玩家出拳
     socket.on("choice", (choice) => {
 
         players[socket.id] = choice;
 
-        let ids = Object.keys(players);
+        console.log(socket.id + " 出了 " + choice);
 
+        const ids = Object.keys(players);
+
+        // 兩人都出拳後判定
         if (ids.length >= 2) {
 
-            let p1 = players[ids[0]];
-            let p2 = players[ids[1]];
+            const p1 = players[ids[0]];
+            const p2 = players[ids[1]];
 
             let result = "";
 
             if (p1 === p2) {
+
                 result = "平手";
-            }
-            else if (
+
+            } else if (
                 (p1 === "石頭" && p2 === "剪刀") ||
                 (p1 === "剪刀" && p2 === "布") ||
                 (p1 === "布" && p2 === "石頭")
             ) {
+
                 result = "玩家1獲勝";
-            }
-            else {
+
+            } else {
+
                 result = "玩家2獲勝";
+
             }
 
             io.emit("result", {
@@ -48,10 +68,34 @@ io.on("connection", (socket) => {
                 result
             });
 
+            // 清空本局資料
             players = {};
         }
+
+    });
+
+    // 玩家離線
+    socket.on("disconnect", () => {
+
+        console.log("玩家離線:", socket.id);
+
+        delete players[socket.id];
+
+        onlinePlayers--;
+
+        if (onlinePlayers < 0) {
+            onlinePlayers = 0;
+        }
+
+        io.emit("onlineCount", onlinePlayers);
+
     });
 
 });
 
-server.listen(process.env.PORT || 3000);
+// Render 使用 PORT
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("伺服器啟動成功 Port:", PORT);
+});
