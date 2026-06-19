f546e6b90e8b92dac160b4e50e38c07b613da050
ken08220729-hub <ken08220729@gmail.com>
2026-06-19 18:18:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send("伺服器正常");
 });
